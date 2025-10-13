26eeae909b02fdff1f33e6cd8d16724572ebfe6a
Carlos DaLomba <carlos@lookingglassfactory.com>
2025-10-13 11:47:55 -0500

    Updated to set resultsTextArea text with file results! +Slight layout improvements

--- gui/firstbasics.py
+++ gui/firstbasics.py
@@ -15,88 +15,66 @@ def readFile(path: str) -> str:
         with open(path, mode="r", encoding="utf-8") as f:
             return f.read()
     except UnicodeDecodeError:
-        print("[WARNING] Skipping file (can't decode in UTF-8): " + path)
+        # print("[WARNING] Skipping file (can't decode in UTF-8): " + path)
         return ""
 
 async def readFileAsync(path: str) -> str:
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(None, readFile, path)
 
-def getFiles(path: str) -> list:
-    innerFiles = []
-    for f in os.listdir(path):
-        fullPath = os.path.join(path, f)
-        if (os.path.isfile(fullPath)):
-            innerFiles.append(f)
-    return innerFiles
-
-def getFilesRecursively(path: str) -> list:
-    relativeFiles = []
-    for subfolder, folders, files in os.walk(path):
-        if re.search(r"\.git", subfolder):
-            continue
-        for fileName in files:
-            fullPath = os.path.join(subfolder, fileName)
-            relativePath = os.path.relpath(fullPath, path)
-            relativeFiles.append(relativePath)
-    return relativeFiles
-
 async def searchInFiles(path: str, searchPattern: str) -> list:
-    relativePaths = []
+    absoluteFiles = []
     for innerPath, folders, files in os.walk(path):
         if re.search(r"\.git", innerPath):
             continue
         for fileName in files:
-            fullPath = os.path.join(innerPath, fileName)
-            relativePath = os.path.relpath(fullPath, path)
-            fileText = await readFileAsync(relativePath)
+            fullPath = os.path.join(innerPath, fileName).replace("\\", "/")
+            # relativePath = os.path.relpath(fullPath, path).replace("\\", "/")
+            fileText = await readFileAsync(fullPath)
             if re.search(searchPattern, fileText):
-                relativePaths.append(relativePath)
-    return relativePaths
+                absoluteFiles.append(fullPath)
+    return absoluteFiles
 
 class ExampleGUI:
     def __init__(self):
         self.mainWindow = tk.Tk()
-        self.mainWindow.geometry("800x600")
+        self.mainWindow.geometry("1024x800")
         self.mainWindow.title("Search In Files")
         self.defaultFont="Arial"
-        self.defaultFontSize = 14
+        self.defaultFontSize = 10
 
-        self.label = tk.Label(self.mainWindow, text="Search In Files", font=(self.defaultFont, self.defaultFontSize + 2))
-        self.label.pack(padx=20, pady=10)
+        # FIELDS:
+        self.titleLabel = tk.Label(self.mainWindow, text="Search In Files", font=(self.defaultFont, self.defaultFontSize + 2))
 
         self.mainFrame = tk.Frame(self.mainWindow)
-        # for i in range(2):
-        #     self.mainFrame.columnconfigure(i, weight=1) # NOTE: This makes each column stretch along the x-axis.
+        self.patternLabel = tk.Label(self.mainFrame, text="Pattern", font=(self.defaultFont, self.defaultFontSize))
+        self.patternField = tk.Entry(self.mainFrame, font=(self.defaultFont, self.defaultFontSize))
 
+        self.folderLabel = tk.Label(self.mainFrame, text="Folder", font=(self.defaultFont, self.defaultFontSize))
+        self.folderField = tk.Entry(self.mainFrame, font=(self.defaultFont, self.defaultFontSize))
+
+        self.submitButton = tk.Button(self.mainWindow, text="Search", font=(self.defaultFont, self.defaultFontSize), command=self.onClick)
+        self.resultsTextArea = tk.Text(self.mainWindow, font=(self.defaultFont, self.defaultFontSize))
+
+        # LAYOUT:
+        self.titleLabel.pack(padx=20, pady=10)
         self.mainFrame.columnconfigure(0, weight=0, minsize=150)
         self.mainFrame.columnconfigure(1, weight=1)
 
-        self.patternLabel = tk.Label(self.mainFrame, text="Pattern", font=(self.defaultFont, self.defaultFontSize))
         self.patternLabel.grid(row=0, column=0, sticky="w", padx=(0, 20))
-        self.patternField = tk.Entry(self.mainFrame, font=(self.defaultFont, self.defaultFontSize))
         self.patternField.grid(row=0, column=1, sticky="we")
-
-        self.folderLabel = tk.Label(self.mainFrame, text="Folder", font=(self.defaultFont, self.defaultFontSize))
         self.folderLabel.grid(row=1, column=0, sticky="w", padx=(0, 20))
-        self.folderField = tk.Entry(self.mainFrame, font=(self.defaultFont, self.defaultFontSize))
         self.folderField.grid(row=1, column=1, sticky="we")
+
         self.mainFrame.pack(fill="x", padx=30, pady=(0, 20)) # NOTE: (top, bottom) -- So this is 20px bottom padding.
 
-        self.outputPathsLabel = tk.Label(self.mainFrame, text="Output Paths Type", font=(self.defaultFont, self.defaultFontSize))
-        self.outputPathsLabel.grid(row=2, column=0, sticky="w", padx=(0, 20))
-
-        self.submitButton = tk.Button(self.mainWindow, text="Search", font=(self.defaultFont, self.defaultFontSize), command=self.onClick)
-        self.submitButton.pack()
-
-        self.resultsTextArea = tk.Text(self.mainWindow, font=(self.defaultFont, self.defaultFontSize))
-        self.resultsTextArea.pack(padx=20, pady=20)
+        self.submitButton.pack(ipadx=20)
+        self.resultsTextArea.pack(fill="both", expand=True, padx=20, pady=20) # NOTE: expand=True makes it respond to extra space from window resize events.
 
     def onClick(self):
         # NOTE: "1.0" means Line 1, Index 0.
         self.resultsTextArea.delete("1.0", tk.END)
         self.resultsTextArea.insert("1.0", "Pattern: " + self.patternField.get() + "\nFolder: " + self.folderField.get() + "\n")
-        print("CLICKED!")
 
         # NOTE: Read the current values in the main UI thread:
         pattern = self.patternField.get()
@@ -111,11 +89,21 @@ class ExampleGUI:
         #   and that event loop is able to run asynchronously with our onClickAsync(...).
         threading.Thread(target=lambda: runTask(pattern, folder)).start()
 
+    # TODO: Gracefully handle RuntimeError if we're still running the event loop as the user clicks the Close button (Alt + F4) to quit the program. "Cannot schedule new futures after shutdown"
     async def onClickAsync(self, searchPattern: str, folderPath: str):
         results = await searchInFiles(folderPath, searchPattern)
-        for filePath in results:
-            print("filePath = " + filePath)
+        
+        def updateUIResults():
+            self.resultsTextArea.delete("1.0", tk.END)
+            for filePath in results:
+                self.resultsTextArea.insert(tk.END, filePath + "\n")
 
+        # NOTE: This uses TKinter's .after() method to schedule something to
+        #   happen next on the main UI thread, where we can safely access TKinter GUI!
+        self.mainWindow.after(0, updateUIResults)
 
     def run(self):
-        self.mainWindow.mainloop()
+        try:
+            self.mainWindow.mainloop()
+        except KeyboardInterrupt:
+            return
